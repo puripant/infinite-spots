e5ff101afbd7daeb7132b3ef79da5dce072cb016
Puripant <puripant@gmail.com>
2018-12-10 02:44:14 +0700

    Generate colors according to r/3 -2g/3 +b/3 = 1 with fuzziness

--- main.js
+++ main.js
@@ -13,7 +13,21 @@ function load() {
   for (let i = 0; i < 1000; i++) {
     let spot = document.createElement('div');
     spot.className = 'spot';
-    spot.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    let red = rand(0, 255);
+    let blue = rand(0, 255);
+    let green = constrain(Math.floor((red + blue) / 2) + rand(0, 100), 0, 255); //from x/3 + y/3 -2z/3 = 0
+    let color_string = `rgb(${constrain(red + rand(0, 100), 0, 255)}, ${green}, ${constrain(blue + rand(0, 100), 0, 255)})`;
+    spot.style.backgroundColor = color_string;
+    // hue: 0/360 = red, 120 = green, 240 = blue
+    // spot.style.backgroundColor = `hsl(${rand(0, 360)}, ${rand(30, 100)}%, ${rand(20, 100)}%)`;
+    // spot.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
     rows.appendChild(spot);
   }
+}
+function rand(min, max) {
+  return min + Math.floor(Math.random() * (max - min + 1));
+}
+function constrain(value, min, max) {
+  return Math.min(Math.max(value, min), max);
 }
