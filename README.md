fe1e8990493f38ccfeeedda1dcfc086d70d862f1
Puripant <puripant@gmail.com>
2018-12-11 13:51:58 +0700

    Adjust colors; add title

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Infinite scrolling of generated spot painting, originally by Damien Hirst. According to [Quantixed](http://quantixed.org/2018/03/26/paintballs-coming-home-generating-damien-hirst-spot-paintings/), the color palette of the artist's spots (at least in "Abalone Acetone Powder") looks like a plane in RGB color space, specifically:
 
 ```
-R/3 - 2*G/3 + B/3 = 0
+R - 2*G + B = 0
 ```
 
 In other words, Damien Hirst does not seem to like bright greens (low R and B but high G) or magenta (high R and B but low G). With some fuzziness added, the result looks like this:
--- main.js
+++ main.js
@@ -13,10 +13,10 @@ function load() {
   for (let i = 0; i < 1000; i++) {
     let spot = document.createElement('div');
     spot.className = 'spot';
-    let red = rand(0, 255);
-    let blue = rand(0, 255);
-    let green = constrain(Math.floor((red + blue) / 2) + rand(0, 100), 0, 255); //from x/3 + y/3 -2z/3 = 0
-    let color_string = `rgb(${constrain(red + rand(0, 100), 0, 255)}, ${green}, ${constrain(blue + rand(0, 100), 0, 255)})`;
+    let red = rand(50, 255);
+    let blue = rand(50, 255);
+    let green = constrain(Math.floor((red + blue) / 2) + rand(-50, 50), 0, 255); //from x/3 + y/3 -2z/3 = 0
+    let color_string = `rgb(${constrain(red + rand(-50, 50), 0, 255)}, ${green}, ${constrain(blue + rand(-50, 50), 0, 255)})`;
     spot.style.backgroundColor = color_string;
     // hue: 0/360 = red, 120 = green, 240 = blue
     // spot.style.backgroundColor = `hsl(${rand(0, 360)}, ${rand(30, 100)}%, ${rand(20, 100)}%)`;
